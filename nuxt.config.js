a0524d8ea2cc6a6158f24ad2fb00fc5d4d6b0b81
yutoc11 <kikuchi-yuto@sagojo.link>
2021-03-28 19:01:23 +0900

    vue-audio-visualをひとまず導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
+  // ssr: false,
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
@@ -25,8 +26,9 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/firebase' },
-    { src: "~plugins/persistedstate.js", ssr: false },
+    { src: "~plugins/persistedstate.js", mode: 'client' },
     { src: '~/plugins/sliderComponent.js', mode: 'client'},
+    { src: '~/plugins/audio-visual.js', mode: 'client'},
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -58,5 +60,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: ['vue-audio-visual'],
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -29,19 +29,25 @@
         .change-button.underline-link.confirm-button(v-if="coverPreviewImage" @click="fileUpload('cover')") 変更確定
 
     section.tw-mx-auto.tw-my-5
-      .audio-upload
-        //- (v-if="!coverImage && !isLoadingCover")
-        .audio-upload-icon-wrapper
-          .audio-upload-icon
-            span
-              svg(viewBox="0 0 24 24")
-                path(:d="iconFileMusic")
-        input.input-file(@change="fileInput($event)" type="file")
-        .change-button.tw-mx-auto.tw-flex.tw-justify-center
-          button.tw-bg-blue-500.tw-text-white.tw-px-4.tw-py-2.tw-my-2.tw-rounded-full.tw-select-none(
-            class="hover:tw-opacity-75"
-            v-if="isSetFile" @click="fileUpload()"
-            ) Cloud Storageに保存、何分割？
+      .audio-upload-wrapper
+        .audio-upload(v-if="!inputFile")
+          //- (v-if="!coverImage && !isLoadingCover")
+          .audio-upload-icon-wrapper
+            .audio-upload-icon
+              span
+                svg(viewBox="0 0 24 24")
+                  path(:d="iconFileMusic")
+          input.input-file(@change="fileInput($event)" type="file")
+        .cancel-button.tw-mx-auto.tw-flex.tw-justify-center(v-else)
+            button.tw-bg-blue-500.tw-text-white.tw-px-4.tw-py-2.tw-my-2.tw-rounded-full.tw-select-none(
+              class="hover:tw-opacity-75"
+              v-if="isSetFile" @click="cancelFile()"
+              ) ファイルをリセット
+      .change-button.tw-mx-auto.tw-flex.tw-justify-center
+        button.tw-bg-blue-500.tw-text-white.tw-px-4.tw-py-2.tw-my-2.tw-rounded-full.tw-select-none(
+          class="hover:tw-opacity-75"
+          v-if="isSetFile" @click="fileUpload()"
+          ) Cloud Storageに保存、何分割？
     section.tw-mx-auto.tw-my-5
       .no-split-length(v-if="currentData.splitLength == 0") 分割してないよ！
       div(v-else).tw-mx-auto
@@ -59,23 +65,35 @@
       .tw-mx-auto(v-for="(item, index) in splitDataArray" :key="index")
         .test.tw-mb-2.tw-text-center {{ item.description }}
 
-    //- section
-    //-   .bar-wrapper
-    //-     .moving-bar.tw-mx-auto.tw-py-20.tw-px-20
-    //-       vue-slider(v-model="value" :min-range="0" :tooltip="'none'" )
-    //-         template(v-slot:process="{ start, end, style, index }")
-    //-           .vue-slider-process(:style="style")
-    //-             .merge-tooltip.vue-slider-dot-tooltip-inner.vue-slider-dot-tooltip-inner-top(v-if="index==0") {{ value[index+1] }} - {{ value[index] }}
-    //-     .wave-test
-    //-     .test-audio-source
-    //-       audio(controls autoplay type="audio/mpeg" src="https://firebasestorage.googleapis.com/v0/b/asas-50dbf.appspot.com/o/output4.mp3?alt=media")
+    .bar-wrapper
+      .moving-bar.tw-mx-auto.tw-py-20.tw-px-20
+        vue-slider(v-model="value" :min-range="0" :tooltip="'none'" )
+          template(v-slot:process="{ start, end, style, index }")
+            .vue-slider-process(:style="style")
+              .merge-tooltip.vue-slider-dot-tooltip-inner.vue-slider-dot-tooltip-inner-top(v-if="index==0") {{ value[index+1] }} - {{ value[index] }}
+      //- .test-audio-source
+        audio(controls autoplay type="audio/mpeg" src="https://firebasestorage.googleapis.com/v0/b/asas-50dbf.appspot.com/o/output4.mp3?alt=media")
+
+    av-waveform(
+      v-if="writeWave"
+      caps-color="#FFF"
+      canv-class="audio-canvas"
+      audio-class="audio-control"
+      :bar-color="['#f00', '#ff0', '#0f0']"
+      canv-fill-color="#000"
+      :caps-height="2"
+      :audio-src="waveAudio"
+      canv-width="600"
+      canv-height="50"
+      )
+    section
+      .test#audio-canvas うふふ
 </template>
 <script lang="js">
 import { mdiFileMusicOutline } from '@mdi/js';
 import firebase from '~/plugins/firebase'
 // import store from '~/store/index.js'
 import { mapActions, mapState, mapGetters } from 'vuex'
-// import WaveSurferVue from "wavesurfer.js-vue";
 
 export default {
 
@@ -97,7 +115,7 @@ export default {
         isLoadingCover: true,
         iconFileMusic: mdiFileMusicOutline,
         isSetFile: false,
-        inputFile: {name:''},
+        inputFile: '',
         currentData: {
           splitLength: 0,
           userId: '',
@@ -110,8 +128,11 @@ export default {
         value: [0, 100],
         // process: val => [ [val[0], val[1]] ]
         // wavesurferのテスト
-        // options: {},
-        // file: "http://example.com/file.mp3",
+        options: {
+        },
+        // file: 'output9.mp3',
+        waveAudio: null,
+        writeWave: false,
       }
   },
 
@@ -143,14 +164,56 @@ export default {
     })
   },
 
+  mounted(){
+
+    const storage = firebase.storage()
+    const storageRef = storage.ref()
+
+    // storageRef.child('output4.mp3').getDownloadURL().then((url) => {
+    //   console.log("中はいった！")
+    //   // `url` is the download URL for 'images/stars.jpg'
+
+    //   // This can be downloaded directly:
+    //   var xhr = new XMLHttpRequest();
+    //   xhr.responseType = 'blob';
+    //   xhr.onload = function(event) {
+    //     var blob = xhr.response;
+    //   };
+    //   xhr.open('GET', url);
+    //   xhr.send();
+
+    //   // Or inserted into an <img> element:
+    //   this.waveAudio = url;
+    //   console.log(url)
+    //   this.nextTick(() => {
+    //     console.log('再描写');  // hello   DOM更新後にこのコードに到達する 
+    //   });
+    // }).catch((error) => {
+    //   // Handle any errors
+    //   console.log('エラー')
+    // });
+  },
+
   methods: {
 
-    fileInput(){
+    cancelFile() {
+      this.inputFile = ''
+      this.waveAudio = null
+      this.isSetFile = false
+      this.writeWave = false
+    },
+
+    fileInput(e){
+
       // アップロード対象は1件のみとする
       console.log('fileInput発火なう')
       this.isSetFile = true;
       this.inputFile = (e.target.files || e.dataTransfer.files)[0];
       console.log(this.inputFile);
+      this.waveAudio = URL.createObjectURL(this.inputFile)
+      console.log(this.waveAudio)
+      this.writeWave = true
+      console.log(this.writeWave)
     },
 
     fileUpload() {
@@ -240,6 +303,10 @@ export default {
   position: absolute;
   bottom: 11px;
   background: rgba(14, 165, 233, 0.1);
+}
+
+.audio-control {
+  display: none;
 }
 
 
